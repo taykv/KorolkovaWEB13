3bf0c37ccbf297997cf63e760a64cb440615b02b
kira <korolkovakv@gmail.com>
2023-01-20 19:42:35 +0700

    Добавьте файлы проекта.

---
+++ Lab13/Models/QuizAnswerModel.cs
@@ -0,0 +1,8 @@
+﻿using Lab13.Utils;
+
+namespace Lab13.Models {
+    public class QuizAnswerModel {
+        [LessThanOrNull(101, ErrorMessage = "Здесь не может быть такого большого числа")]
+        public int? Answer { get; set; }
+    }
+}
